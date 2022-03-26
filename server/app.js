a819ca7851d0ef0c35dbe4fc619e1614504ba582
ryan-neil <ryanjosephneil@gmail.com>
2022-03-26 07:02:57 -1000

    update: folder structure

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+// imports
+require('dotenv').config();
+const tiles = require('./routes/tiles');
+const connectDB = require('./db/connect');
+const notFound = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
+
+// serve middleware
+app.use(express.static('./client'));
+app.use(express.json());
+
+// serve routes
+app.use('/api/v1/tiles', tiles); // all tile routes
+app.use(notFound); // 404
+app.use(errorHandlerMiddleware); // custom error handler middleware
+
+// DJ, spin that sh*t...
+const start = async () => {
+  const { PORT } = process.env || 9001;
+  const { DATABASE_URI } = process.env;
+
+  try {
+    // connect to database
+    await connectDB(DATABASE_URI);
+    // fire up the server once we successfully connect to the database
+    app.listen(PORT, () =>
+      console.log(`Server listening at: http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
